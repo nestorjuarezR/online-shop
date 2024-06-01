80d7d436dadc83b8db4eb746d1ca331d5633bbbf
Nestor <nesk.company@gmail.com>
2024-06-01 13:48:06 -0600

    Create Category and Product model

---
+++ ecommerce/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+#Model Category
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+                            models.Index(fields=['name']),
+                            ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+    
+
+
+#Model Product
+class Product(models.Model):
+    category = models.ForeignKey(Category,                                                       #Foreing Key
+                                                                related_name='products',
+                                                                on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug  = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', 
+                                                            blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,
+                                                            decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes =  [
+                                models.Index(fields=['id', 'slug']),
+                                models.Index(fields=['name']),
+                                models.Index(fields=['-created']),
+                            ]       
+    def __str__(self):
+        return self.name
+    
