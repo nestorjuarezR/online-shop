d8645d83eb60f680e473b6870057f87499dc875b
Nestor <nesk.company@gmail.com>
2024-06-01 20:46:02 -0600

    Create cart_add, cart_remove and set the urls in cart app

--- shop/urls.py
+++ shop/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ecommerce.urls')),
+    path('cart/', include('cart.urls')),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
