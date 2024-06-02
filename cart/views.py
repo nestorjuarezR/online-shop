fc9ed189f919bd0d602e5216a4989f671a8bdcae
Nestor <nesk.company@gmail.com>
2024-06-02 15:12:22 -0600

    Fix bugs in the card add functions

--- cart/views.py
+++ cart/views.py
@@ -1,28 +1,23 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
-from ecommerce.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
-
+from ecommerce.models import Product
 
 @require_POST
 def cart_add(request, product_id):
-
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
-
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(product=product,
-                        quantity=cd['quantity'],
-                        override_quantity=cd['override'])
+        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
     return redirect('cart:cart_detail')
 
 @require_POST
 def cart_remove(request, product_id):
     cart = Cart(request)
-    product = get_object_or_404(Product, id = product_id)
+    product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
 
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
-
+from cart.forms import CartAddProductForm
 
 
 
@@ -24,6 +24,8 @@ def product_detail(request, id, slug):
                                                                 id=id,
                                                                 slug = slug,
                                                                 available = True)
+    cart_product_form = CartAddProductForm()
     return render(request,
                                 'ecommerce/detail.html',
-                                {'product': product})
+                                {'product': product,
+                                 'cart_product_form': cart_product_form})
